51d55be759f4925a580edda52710332a0e2ab41c
Keshav Rajput <keshav.rajput@walmart.com>
2020-03-05 20:10:24 -0600

    initial commit

---
+++ src/main/java/com/cricket/ipl2020/Ipl2020Application.java
@@ -0,0 +1,13 @@
+package com.cricket.ipl2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipl2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipl2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricket/ipl2020/Ipl2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.ipl2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ipl2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
